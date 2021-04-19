93e40c72b0d38b9f2f2b51302a99e95a2b9f442d
mark-haskins11792 <44908850+mark-haskins11792@users.noreply.github.com>
2021-04-18 22:13:50 -0500

    Delete Assignment 0902_Game of Life.py

--- Assignment 0902_Game of Life.py
+++
@@ -1,5 +0,0 @@
-# Name: Mark Haskins
-# Date: 3/8/2021
-# Honor Statement: I have not given or received any unauthorized assistance on this assignment
-# Title: Assignment 0902: Game of Life
-# Link to video: 
